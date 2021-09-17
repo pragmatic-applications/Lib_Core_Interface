6950c0c1065bb3d0d96b8eada67df285d78bb8ec
Maurice Kelly <kay86@live.com>
2021-09-17 16:18:50 +0100

    Update to .NET 6

--- docs/Record.cs
+++ docs/Record.cs
@@ -1,11 +1,11 @@
 //=================================================================
-// Solution: Lib_Core_Interface
+// Solution: MAK.Lib.Core.Interface
 //=================================================================
 // Programmer: Maurice Antonio Kelly @MAKMaurice
 // Description: A To Do App with undo, redo and database CRUD features.
-// ASP.NET Core 5.0
-// Date: 2021-08-14
-// Stop: 11:00
+// ASP.NET Core 6.0
+// Date: 2021-09-17
+// Stop: 16:00
 //=================================================================
 
 
