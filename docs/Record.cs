ea48cada009898df0aac14e677e0145f644c8d60
Maurice <makmaurice@msn.com>
2025-01-22 10:56:26 +0000

    Update from 6 to 9

--- docs/Record.cs
+++ docs/Record.cs
@@ -3,15 +3,10 @@
 //=================================================================
 // Programmer: Maurice Antonio Kelly @MAKMaurice
 // Description: A To Do App with undo, redo and database CRUD features.
-// ASP.NET Core 6.0
-// Date: 2021-09-17
-// Stop: 16:00
+
+// Date: 2025-01-22
+// Stop: 11:00
+// Git Message: Update from 6 to 9
 //=================================================================
 
-
-/*
-
-
-
-//*/
 
